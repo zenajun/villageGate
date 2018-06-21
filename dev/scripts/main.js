1c5be73912894ed1fdd6a5eed4d425af79ee50a5
Zena Jun <zena.jun@gmail.com>
2018-06-21 14:46:01 -0400

    Adding content to index

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -5,3 +5,48 @@ const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,
 
 document.getElementById('sidebar').style.height = `${height}px`;
 
+const app = {};
+
+// app.hamburger = () => {
+//   $('#nav-icon3').click(() => {
+//     $('ul').toggleClass('active');
+//   });
+// }
+
+// app.closeHamburger = () => {
+//   $('.header__nav__list__item__link').click(() => {
+//     $('ul').removeClass('active');
+//     $('#nav-icon3').removeClass('open');
+//   });
+// }
+
+// app.hamburgerAnimate = () => {
+//   $('#nav-icon3').click(function () {
+//     $(this).toggleClass('open');
+//   });
+// }
+
+app.smoothScroll = () => {
+  $("a").on('click', function (e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      const hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+}
+
+app.init = () => {
+  app.smoothScroll();
+  // app.hamburger();
+  // app.closeHamburger();
+  // app.hamburgerAnimate();
+}
+
+$(function () {
+  app.init();
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,3 +6,49 @@ var html = document.documentElement;
 var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
 document.getElementById('sidebar').style.height = height + 'px';
+
+var app = {};
+
+// app.hamburger = () => {
+//   $('#nav-icon3').click(() => {
+//     $('ul').toggleClass('active');
+//   });
+// }
+
+// app.closeHamburger = () => {
+//   $('.header__nav__list__item__link').click(() => {
+//     $('ul').removeClass('active');
+//     $('#nav-icon3').removeClass('open');
+//   });
+// }
+
+// app.hamburgerAnimate = () => {
+//   $('#nav-icon3').click(function () {
+//     $(this).toggleClass('open');
+//   });
+// }
+
+app.smoothScroll = function () {
+  $("a").on('click', function (e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+};
+
+app.init = function () {
+  app.smoothScroll();
+  // app.hamburger();
+  // app.closeHamburger();
+  // app.hamburgerAnimate();
+};
+
+$(function () {
+  app.init();
+});
